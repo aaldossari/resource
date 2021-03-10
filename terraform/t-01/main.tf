fa06a6839e1cd9fd70b13c17933de11ffd5b3d7c
aaldossari <aaldossari@hawk.iit.edu>
2021-03-10 04:19:18 -0600

    Update terraform provider's version

--- terraform/t-01/main.tf
+++ terraform/t-01/main.tf
@@ -1,11 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
 provider "aws" {
-  version = "2.65.0"
+  profile = "default"
   region  = "us-west-2"
 }
 
 resource "aws_instance" "marajea" {
-  ami           = "ami-09dd2e08d601bff67"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+
   tags = {
     Name = "Marajea"
   }
--- terraform/t-02/main.tf
+++ terraform/t-02/main.tf
@@ -1,22 +1,21 @@
-provider "aws" {
-  version = "2.65.0"
-  region = "us-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"]
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "marajea" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0ca5c3bd5a268e7db"
   instance_type = "t2.micro"
+
   tags = {
     Name = "Marajea"
   }
